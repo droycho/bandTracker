5ee325b241a76797a4c2c810809d8cd2e897bb3a
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-05-13 11:10:06 -0700

    pass first integration test.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,15 +6,16 @@ import java.util.List;
 
 public class App {
   public static void main(String[] args) {
-    // staticFileLocation("/public");
-    // String layout = "templates/layout.vtl";
-    //
-    // get("/", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/index.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    // For rootTest in AppTest
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     // get("/venues", (request, response) -> {
     //   HashMap<String, Object> model = new HashMap<String, Object>();
     //   model.put("venues", Venue.all());
